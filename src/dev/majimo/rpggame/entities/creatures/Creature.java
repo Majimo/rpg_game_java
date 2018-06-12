ac4aa73c73ef80b7197ccfca1c66e0c11ff76405
Pierre FERVEL <pierre.fervel@gmail.com>
2018-06-12 22:55:53 +0200

    Improving Player moves

--- src/dev/majimo/rpggame/entities/creatures/Creature.java
+++ src/dev/majimo/rpggame/entities/creatures/Creature.java
@@ -4,11 +4,58 @@ import dev.majimo.rpggame.entities.Entity;
 
 public abstract class Creature extends Entity {
 	
+	public static final int DEFAULT_HEALTH = 10;
+	public static final float DEFAULT_SPEED = 3.0f;
+	public static final int DEFAULT_CREATURE_WIDTH = 128;
+	public static final int DEFAULT_CREATURE_HEIGHT = 128;
+	
 	public int health;
+	protected float speed;
+	protected float xMove, yMove;
 
-	public Creature(float x, float y) {
-		super(x, y);
-		health = 10;
+	public Creature(float x, float y, int width, int height) {
+		super(x, y, width, height);
+		health = DEFAULT_HEALTH;
+		speed = DEFAULT_SPEED;
+		xMove = 0;
+		yMove = 0;
+	}
+	
+	public void move() {
+		x += xMove;
+		y += yMove;
+	}
+
+	public int getHealth() {
+		return health;
+	}
+
+	public void setHealth(int health) {
+		this.health = health;
+	}
+
+	public float getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(float speed) {
+		this.speed = speed;
+	}
+
+	public float getxMove() {
+		return xMove;
+	}
+
+	public void setxMove(float xMove) {
+		this.xMove = xMove;
+	}
+
+	public float getyMove() {
+		return yMove;
+	}
+
+	public void setyMove(float yMove) {
+		this.yMove = yMove;
 	}
 
 }
